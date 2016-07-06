868cabd4273174701a8b31c5298c0b016f9d7ec3
timeropen <timer_open@163.com>
2016-07-06 19:02:42 +0800

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
 } from 'react-native';
 
 class HelloWorld extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+       dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded:false,
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+ componentDidMount(){
+  this.fetchData();
+}
+ fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded:true,
+        });
+      })
+      .done();
+  }
+   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <ListView dataSource={this.state.dataSource} 
+                renderRow ={this.renderMovie}
+                style={styles.listView}
+      />
+      );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          正在加载电影数据……
         </Text>
       </View>
     );
   }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}/>
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+
+ 
 }
 
-const styles = StyleSheet.create({
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer:{
+    flex:1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail:{
+    width:53,
+    height:81,
   },
+  title:{
+      fontSize:20,
+      marginBottom:8,
+      textAlign:'center',
+  },
+  year:{
+      textAlign:'center',
+  },
+  listView:{
+    paddingTop:20,
+    backgroundColor:'#F5FCFF',
+  }
 });
 
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+
